d393ea040abbdef52d512910d572f0a7aa8c6d1a
luizsilva <luizfsilva.091595@gmail.com>
2021-08-11 10:42:09 -0400

    initialized app created post and get endpoints

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    app.listen('5000', () => {
+      console.log('Listening on http://localhost:5000...')
+    })
+  })
+  .catch(err => console.log(err))
+
+const UserSchema = mongoose.Schema(
+  {
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+)
+
+const UsersModel = mongoose.model('Users', UserSchema)
+
+app.get('/users', (req, res) => {
+  UsersModel.find()
+    .then(allUsers => res.status(200).send(allUsers))
+    .then(() => console.log('all users sent'))
+    .catch(err => console.log(err))
+})
+
+app.post('/users', (req, res) => {
+  console.log('this is req body', req.body)
+  new UsersModel(req.body)
+    .save()
+    .then(() => {
+      UsersModel.findOne({ email: req.body.email})
+      .then(foundUser => res.send(foundUser))
+    })
+    .catch(err => console.log(err))
+})
+
+app.post('/login', (req, res) => {
+  UsersModel.findOne({ email: req.body.email })
+    .then(userFound => {
+      console.log(userFound)
+      if (!userFound) {
+        return res.status(404).send('User Not found')
+      }
+
+      if (userFound && userFound.password === req.body.password) {
+        res.status(200).send('user is good to go')
+      } else {
+        res.status(404).send('User not authenticated')
+      }
+    })
+
+    .catch(err => console.log(err))
+})
